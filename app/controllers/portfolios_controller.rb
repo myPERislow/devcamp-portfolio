fb788118fd1a1013ebaf6bac9fc91eab6f49c49f
Takumi Sato <satotaku.kamigou@gmail.com>
2018-01-20 01:17:39 +0900

    editとupdate機能を追加

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,4 +20,21 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'ブログの更新に成功しました！' }
+        format.json { render :show, status: :ok, location: @portfolio_title }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
