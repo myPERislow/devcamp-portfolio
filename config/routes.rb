83fc77a8551ad2a88a443c97690d52f1a40041d0
Takumi Sato <satotaku.kamigou@gmail.com>
2018-01-20 02:42:04 +0900

    portfolio routesを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
-
-  get 'about-me', to: 'pages#about'
-  get 'asdfasdf', to: 'pages#contact'
-
+  resources :portfolios, except: [:show]
+  get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
-
   root to: 'pages#home'
 end
